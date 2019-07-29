fdeec4acb463236e8a82e1d1308b5a32ef363159
yuweitseng1993 <s2734d512@gmail.com>
2019-07-28 20:03:13 -0400

    added third activity that allows users to enter their personal info (after created the account)

--- app/src/main/java/com/example/androidassignment1/CreateAccount.java
+++ app/src/main/java/com/example/androidassignment1/CreateAccount.java
@@ -2,9 +2,11 @@ package com.example.androidassignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -38,15 +40,18 @@ public class CreateAccount extends AppCompatActivity {
 
         upper_err = false;
         bottom_err = false;
+        final Drawable tick = getResources().getDrawable(R.drawable.tick);
 
         btn_nxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(upper_err == true){
                     rl_invalid_email_fmt.setVisibility(View.GONE);
+                    upper_err = false;
                 }
                 if(bottom_err == true){
                     rl_nonmatch_pswd_err.setVisibility(View.GONE);
+                    bottom_err = false;
                 }
                 String usr_email = et_email_addr.getText().toString();
                 String usr_pswd = et_crt_pswrd.getText().toString();
@@ -58,6 +63,7 @@ public class CreateAccount extends AppCompatActivity {
                     rl_nonmatch_pswd_err.setVisibility(View.VISIBLE);
                     tv_btm_err_mes = findViewById(R.id.tv_pswd_error_mes);
                     tv_btm_err_mes.setText("One or more fields is empty");
+                    // -------- change later: should detect which field is filled, keep it --------
                     et_email_addr.setText("");
                     et_crt_pswrd.setText("");
                     et_rpt_pswrd.setText("");
@@ -65,27 +71,25 @@ public class CreateAccount extends AppCompatActivity {
                 }
                 else{
                     //verify user input
-                    if(validate_email_addr(usr_email) == true){
+                    if(validate_email_addr(usr_email)){
                         //Green boarder for email EditText + green tick
-                        Drawable tick = getResources().getDrawable(R.drawable.tick);
                         et_email_addr.setCompoundDrawablesWithIntrinsicBounds(null, null, tick, null);
                         et_email_addr.setBackgroundResource(R.drawable.popup_cormes_border);
 
                         //validates password
-//                        validate_pswd(usr_pswd, usr_rpt_pswd, true);
+                        validate_pswd(usr_pswd, usr_rpt_pswd);
                     }
                     else{
-                        //Error: invalid email address
+                        //ERROR: invalid email address
                         rl_invalid_email_fmt.setVisibility(View.VISIBLE);
+                        et_email_addr.setBackgroundResource(R.drawable.popup_erret_border);
                         tv_up_err_mes = findViewById(R.id.tv_email_error_mes);
                         tv_up_err_mes.setText("Invalid email format");
+                        upper_err = true;
 
                         //validates password
-//                        validate_pswd(usr_pswd, usr_rpt_pswd, false);
+                        validate_pswd(usr_pswd, usr_rpt_pswd);
                         et_email_addr.setText("");
-                        et_crt_pswrd.setText("");
-                        et_rpt_pswrd.setText("");
-                        upper_err = true;
                     }
                 }
             }
@@ -95,28 +99,97 @@ public class CreateAccount extends AppCompatActivity {
                 return email.matches(regex);
             }
 
-//            void validate_pswd(String pswd_1, String pswd_2, boolean email_cond){
-//                if(email_cond == true){
-//                    if(is_valid_pswd(pswd_1) && is_valid_pswd(pswd_2)){
-//                        if(pswd_1.equals(pswd_2)){
-//
-//                        }
-//                        else{
-//
-//                        }
-//                    }
-//                    else{
-//
-//                    }
-//                }
-//                else{
-//
-//                }
-//            }
-//
-//            boolean is_valid_pswd(String pswd){
-//
-//            }
+            void validate_pswd(String pswd_1, String pswd_2){
+                if(is_valid_pswd(pswd_1) && is_valid_pswd(pswd_2)){
+                    if(pswd_1.equals(pswd_2)){
+                        //everything matches, proceed to next intent
+                        et_crt_pswrd.setCompoundDrawablesWithIntrinsicBounds(null, null, tick, null);
+                        et_crt_pswrd.setBackgroundResource(R.drawable.popup_cormes_border);
+                        et_rpt_pswrd.setCompoundDrawablesWithIntrinsicBounds(null, null, tick, null);
+                        et_rpt_pswrd.setBackgroundResource(R.drawable.popup_cormes_border);
+
+                        //proceed to next intent only when all matches
+                        if(!upper_err){
+                            try {
+                                Thread.sleep(2000);
+                            }catch (Exception e){
+                                e.printStackTrace();
+                            }
+                            //triggers EnterUserInfo intent when button clicked (after all input info is verified)
+                            Intent intent = new Intent(getBaseContext(), EnterUserInfo.class);
+                            //move to EnterUserInfo class for input personal info
+                            startActivity(intent);
+                        }
+
+                    }
+                    else{
+                        Log.i("tag", "passwords don't match");
+                        //ERROR: passwords are valid but do not match
+                        et_crt_pswrd.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
+                        et_crt_pswrd.setBackgroundResource(R.drawable.popup_erret_border);
+                        et_rpt_pswrd.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
+                        et_rpt_pswrd.setBackgroundResource(R.drawable.popup_erret_border);
+
+                        rl_nonmatch_pswd_err.setVisibility(View.VISIBLE);
+                        tv_btm_err_mes = findViewById(R.id.tv_pswd_error_mes);
+                        tv_btm_err_mes.setText("Your passwords don't match");
+//                        et_email_addr.setText("");
+                        et_crt_pswrd.setText("");
+                        et_rpt_pswrd.setText("");
+                        bottom_err = true;
+                    }
+                }
+                else{
+                    Log.i("tag", "passwords are invalid");
+                    //ERROR: one or more passwords is invalid
+                    et_crt_pswrd.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
+                    et_crt_pswrd.setBackgroundResource(R.drawable.popup_erret_border);
+                    et_rpt_pswrd.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
+                    et_rpt_pswrd.setBackgroundResource(R.drawable.popup_erret_border);
+
+                    rl_nonmatch_pswd_err.setVisibility(View.VISIBLE);
+                    tv_btm_err_mes = findViewById(R.id.tv_pswd_error_mes);
+                    tv_btm_err_mes.setText("Invalid password(s) entered");
+//                    et_email_addr.setText("");
+                    et_crt_pswrd.setText("");
+                    et_rpt_pswrd.setText("");
+                    bottom_err = true;
+                }
+            }
+
+            boolean is_valid_pswd(String pswd){
+                if(pswd.length() < 8){
+                    return false;
+                }
+                else{
+                    char[] chars = pswd.toCharArray();
+                    boolean has_num, has_upper, has_lower;
+                    has_num = has_upper = has_lower = false;
+                    for(char a : chars){
+                        if(!has_num){
+                            if(Character.isDigit(a)){
+                                has_num = true;
+                            }
+                        }
+                        if(!has_upper){
+                            if(Character.isUpperCase(a)){
+                                has_upper = true;
+                            }
+                        }
+                        if(!has_lower){
+                            if(Character.isLowerCase(a)){
+                                has_lower = true;
+                            }
+                        }
+                    }
+                    if(has_num && has_upper && has_lower){
+                        return true;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
         });
 
     }
---
+++ app/src/main/java/com/example/androidassignment1/EnterUserInfo.java
@@ -0,0 +1,132 @@
+package com.example.androidassignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+import static java.lang.Integer.parseInt;
+
+public class EnterUserInfo extends AppCompatActivity {
+    EditText et_name, et_u_name, et_usr_pswd, et_age, et_addr;
+    Button btn_sv, btn_bir_pick;
+    RadioGroup rad_gp;
+    RadioButton rad_btn;
+    Spinner spin_country;
+
+    String gender, country, address;
+    DatePickerDialog datePickerDialog;
+    int year, picked_year, month, picked_month, dayOfMonth, picked_dayOfMonth;
+    Calendar calendar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_user_info);
+
+        et_name = findViewById(R.id.et_usr_name);
+        et_u_name = findViewById(R.id.et_usr_username);
+        et_usr_pswd = findViewById(R.id.et_pswd);
+        et_age = findViewById(R.id.et_usr_age);
+        et_addr= findViewById(R.id.et_postal_addr);
+        btn_bir_pick = findViewById(R.id.btn_datepicker);
+        spin_country = findViewById(R.id.sp_country);
+        btn_sv = findViewById(R.id.btn_save);
+        rad_gp = findViewById(R.id.rg_gender);
+        picked_year = picked_month = picked_dayOfMonth = 0;
+        gender = address = "none";
+
+        String[] country_array = new String[] {"Not-Specified", "Malaysia", "United States",
+                "Indonesia", "France", "Italy", "Singapore", "New Zealand", "India"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, country_array);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin_country.setAdapter(adapter);
+
+
+        btn_bir_pick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar = Calendar.getInstance();
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+                datePickerDialog = new DatePickerDialog(EnterUserInfo.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                picked_year = year;
+                                picked_month = month;
+                                picked_dayOfMonth = day;
+//                                Log.i("user_input", "year: " + year + " month: " + month + " day: " + day);
+                            }
+                        }, year, month, dayOfMonth);
+                datePickerDialog.show();
+            }
+        });
+
+        btn_sv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean has_empty_field = false;
+                String name = et_name.getText().toString();
+                String username = et_u_name.getText().toString();
+                String password = et_usr_pswd.getText().toString();
+                String age_str = et_age.getText().toString();
+                country = spin_country.getSelectedItem().toString();
+
+                if(name.length() == 0 || username.length() == 0 || password.length() == 0 || age_str.length() == 0){
+                    has_empty_field = true;
+                }
+                //check if "birth date", "gender", and "country" are specified
+                if(gender.equals("none") || country.equals("Not-Specified")){
+                    has_empty_field = true;
+                }
+                if(picked_year == 0 || picked_month == 0 || picked_dayOfMonth == 0){
+                    has_empty_field = true;
+                }
+
+                if(has_empty_field){
+                    //Toast to warn empty
+                    String err_mes = "One or more empty fields exists";
+                    Toast.makeText(getApplicationContext(), err_mes, Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    //Toast to display user inputs
+                    int age = parseInt(et_age.getText().toString());
+                    String message = "name: " + name + "\nusername: " + username + "\npassword: " + password + "\nage: " + age +
+                            "\nbirth date: " + picked_month + "/" + picked_dayOfMonth + "/" + picked_year + "\ncountry: " + country +
+                            "\ngender: " + gender;
+                    Log.i("user_input", message);
+                    if(address.equals("none")){
+                        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        String message_addr = message + "\naddress: " + address;
+                        Toast.makeText(getApplicationContext(), message_addr, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+    }
+
+    public void checkButton(View view) {
+        int radio_picked = rad_gp.getCheckedRadioButtonId();
+        rad_btn = findViewById(radio_picked);
+        gender = rad_btn.getText().toString();
+    }
+}
